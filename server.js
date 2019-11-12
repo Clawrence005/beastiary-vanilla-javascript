3549acdd6a262ff6a7197cca925d511049d847b4
colin <clawrence005@yahoo.com>
2019-11-12 17:27:33 -0600

    adding packe.json, gitignore

--- server.js
+++ server.js
@@ -1,48 +1,14 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require("express");
+var router = express();
 
+var PORT = process.env.PORT || 3000;
 
-// // this should show the specific page clicked
-// router.get('/monsters/:routeName', function (req, res) {
-//   var monster = dataFile.monsters[req.params.routeName];
-//   res.send(`
-//   <link rel="stylesheet" type="text/css"
-//   href="/css/style.css">
-//   <h1>${monster.name}</h1>
-//   <h2>${monster.location}</h2>
-//   <p>${monster.descrip}<p>
-//   <script src="/reload/reload.js"></script>
-//   `);
-// })
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
 
-// var express = require('express');
-// var router = express.Router();
-// // ... Importations and configurations
-// router.param('postId', function (request, response, next) {
-//   // Find post by ID
-//   // Save post to request
-//   request.post = {
-//     name: 'PHP vs. Node.js',
-//     url: ' http://webapplog.com/php-vs-node-js '
-//   };
-//   return next();
-// });
+require("./routes/apiRoutes")(router);
+require("./routes/htmlRoutes")(router);
 
-// router
-//   .route('/posts/:postId')
-//   .all(function (request, response, next) {
-//     // This will be called for request with any HTTP method
-//   })
-//   .post(function (request, response, next) {
-//   })
-//   .get(function (request, response, next) {
-//     response.json(request.post);
-//   })
-//   .put(function (request, response, next) {
-//     // ... Update the post
-//     response.json(request.post);
-//   })
-//   .delete(function (request, response, next) {
-//     // ... Delete the post
-//     response.json({ 'message': 'ok' });
-//   }) 
+router.listen(PORT, function () {
+  console.log(`App listening on PORT: ${PORT}`)
+})
